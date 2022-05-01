fccfa948c3a5ab49b22be38f795802da9d935500
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-04-30 20:06:08 -0500

    Configuración más básica/minima-funcional del servidor

---
+++ app.js
@@ -0,0 +1,21 @@
+// Requerimos el objeto de express
+const express = require('express');
+
+//App de express 
+const app = express()
+
+//Puerto en el que vamos a ver nuestra app: localhost:30001
+const port = 3001;
+
+
+//Path inicial que responderá a la url localhost:3000
+app.get('/',(req,res)=>{
+    res.send('Hello world');
+})
+
+//Con esto inicializamos esta app
+app.listen(port,()=>{
+    console.log(`Ejemplo de app servidor escuchando en el puerto ${port}`);
+})
+
+
